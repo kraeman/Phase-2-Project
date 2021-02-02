7f06ab44bd489b14d65f906a3b8f013d5fea489d
krae <kraewind@gmail.com>
2021-02-02 13:50:09 -0500

    stub out file structure part 2

---
+++ app/controllers/cake_controller.rb
@@ -0,0 +1,24 @@
+class CakeController < ApplicationController
+    get '/cakes' do
+        erb :'/cakes/index'
+    end
+
+    get '/cakes/new' do
+        erb :'/cakes/new'
+    end
+
+    get '/cakes/:id/edit' do
+        erb :'/cakes/new'
+    end
+
+    get '/cakes/:id' do
+        erb :'/cakes/new'
+    end
+
+    post '/cakes' do
+        @cake = Cake.create()
+    end
+
+    patch '/cakes/:id' do
+        
+    end
