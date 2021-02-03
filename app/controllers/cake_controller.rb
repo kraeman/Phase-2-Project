41fcbd8e91502a7e17f1646c9881f2b181c989f8
krae <kraewind@gmail.com>
2021-02-03 13:12:07 -0500

    wrote part of cake controller

--- app/controllers/cake_controller.rb
+++ app/controllers/cake_controller.rb
@@ -1,22 +1,41 @@
 class CakeController < ApplicationController
     get '/cakes' do
+        @cakes = Cake.all
         erb :'/cakes/index'
     end
 
     get '/cakes/new' do
+        @ingredients = Ingredient.all
+        @people = Person.all
         erb :'/cakes/new'
     end
 
     get '/cakes/:id/edit' do
-        erb :'/cakes/new'
+        @cake = Cake.find()
+        @ingredients = Ingredient.all
+        @people = Person.all
+        erb :'/cakes/edit'
     end
 
     get '/cakes/:id' do
-        erb :'/cakes/new'
+        @cake = Cakes.find()
+        erb :'/cakes/show'
     end
 
     post '/cakes' do
-        @cake = Cake.create()
+        @cake = Cake.create(params["cake"])
+ 
+        unless params[]
+            @cake.titles << Title.create(params["title"])
+        end
+
+        unless params[]
+            @cake.landmarks << Landmark.create(params["landmark"])
+        end
+        
+        @cake.save
+    
+        redirect to "/cakes/#{@cake.id}"
     end
 
     patch '/cakes/:id' do
